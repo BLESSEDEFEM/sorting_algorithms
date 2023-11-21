e93a6dd784cedbd38c14bc7caf91a70476044ed1
Blessing <nejo.blessing@yahoo.com>
2023-11-21 15:54:15 -0800

    sorting algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -23,7 +23,7 @@ void swap_ints(int *a, int *b)
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t, len = size;
+	size_t i, len = size;
 	bool bubbly = false;
 
 	if (array == NULL || size < 2)
@@ -37,6 +37,7 @@ void bubble_sort(int *array, size_t size)
 			if (array[i] > array[i + 1])
 			{
 				swap_ints(array + i, array + i + 1);
+				
 				print_array(array, size);
 				bubbly = false;
 			}
--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -17,7 +17,10 @@ void merge_subarr(int *subarr, int *buff, size_t front, size_t mid, size_t back)
 	size_t i, j, k = 0;
 
 	printf("Merging...\n[left]: ");
-	printf_array(subarr + mid, back - mid);
+	print_array(subarr + front, mid - front);
+
+	printf("[right]: ");
+	print_array(subarr + mid, back - mid);
 
 	for (i = front, j = mid; i < mid && j < back; k++)
 		buff[k] = (subarr[i] < subarr[j]) ? subarr[i++] : subarr[j++];
@@ -66,7 +69,11 @@ void merge_sort(int *array, size_t size)
 
 	if (array == NULL || size < 2)
 		return;
-
+	
+	buff = malloc(sizeof(int) * size);
+	if (buff == NULL)
+		return;
+	
 	merge_sort_recursive(array, buff, 0, size);
 
 	free(buff);
